c21e1465eec9d1357b46ff6946b75fb8355bd7fe
Youri Blanckenborg <0331038@student.rocvantwente.nl>
2022-12-09 13:34:38 +0100

    Fixed routing.

--- Randomizer/Controllers/RandomizerController.0331038.cs
+++ Randomizer/Controllers/RandomizerController.0331038.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Globalization;
 
 namespace Randomizer.Controllers
 {
     public partial class RandomizerController
     {
-        /// <summary>Randoms the default RandomFirstNames view</summary>
+        #region Private Fields
+        private List<string> names = new();
+        private List<SelectListItem> countries = new();
+        #endregion
+        /// <summary>Returns the default RandomFirstNames View</summary>
         /// <returns>
         ///   <br />
         /// </returns>
@@ -56,6 +62,121 @@ namespace Randomizer.Controllers
             }
 
             return View("RandomFirstNames");
+        }
+
+        /// <summary>Returns the default RandomNames View</summary>
+        /// <returns>
+        ///   <br />
+        /// </returns>
+        public IActionResult RandomNames(string country)
+        {
+            try
+            {
+                GetNames(names, country, 0);
+                ViewBag.Countries = countries;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRandomNames(string country, int amountOfNames)
+        {
+            if (country == null)
+            {
+                try
+                {
+                    GetNames(names, country, amountOfNames);
+                    ViewBag.Countries = countries;
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Error = ex.Message;
+                }
+            }
+
+            try
+            {
+                GetNames(names, country, amountOfNames);
+                ViewBag.Names = names;
+                ViewBag.Selected = country;
+                ViewBag.Countries = countries;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View("RandomNames");
+        }
+
+        public List<string> GetNames(List<string> names, string country, int amountOfNames)
+        {
+            bool valid = false;
+
+            List<SelectListItem> countries = new List<SelectListItem>();
+            countries.Add(new SelectListItem("Spanje", "SP"));
+            countries.Add(new SelectListItem("Italië", "IT"));
+            countries.Add(new SelectListItem("Griekenland", "GR"));
+            countries.Add(new SelectListItem("Polen", "PL"));
+            countries.Add(new SelectListItem("Duitsland", "GE"));
+            this.countries = countries;
+
+            foreach (SelectListItem value in countries)
+            {
+                if (value.Value == country)
+                {
+                    value.Selected = true;
+                    valid = true;
+                    break;
+                }
+            }
+
+            if (!valid && country != null)
+            {
+                throw new ArgumentException("This country does not exist in this list.");
+            }
+
+            if (country is not "" or null)
+            {
+                switch (country)
+                {
+                    case "SP":
+                        for (int i = 0; i < amountOfNames; i++)
+                        {
+                            names.Add("Spaanse naam");
+                        }
+                        break;
+                    case "IT":
+                        for (int i = 0; i < amountOfNames; i++)
+                        {
+                            names.Add("Italiaanse naam");
+                        }
+                        break;
+                    case "GR":
+                        for (int i = 0; i < amountOfNames; i++)
+                        {
+                            names.Add("Griekse naam");
+                        }
+                        break;
+                    case "PL":
+                        for (int i = 0; i < amountOfNames; i++)
+                        {
+                            names.Add("Poolse naam");
+                        }
+                        break;
+                    case "GE":
+                        for (int i = 0; i < amountOfNames; i++)
+                        {
+                            names.Add("Duitse naam");
+                        }
+                        break;
+                }
+            }
+            this.names = names;
+            return names;
         }
     }
 }
